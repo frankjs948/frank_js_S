bbd9c9edd0c89bd3b1f2b7ff78ca10ee45b2db6b
Frank <frankjs948@gmail.com>
2025-09-14 16:19:28 +0800

    2509141619

---
+++ 250914_2.js
@@ -0,0 +1,9 @@
+shopCartPrice = 200;
+console.log("hello word");
+// alert("HELLO WORD");
+console.log("網頁載入完成！");
+console.log("今天是：" + new Date().toLocaleString());
+// console.log(a);
+console.warn("HELLO WORD");
+console.error("HELLO WORD");
+console.info("HELLO WORD");
